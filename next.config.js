3d066d79709256057379ccbe8d241b0852509034
anpythomas <anpythomas@users.noreply.github.com>
2023-08-10 03:00:05 +0000

    deploy: Event-Pro-Solutions/client@86258850264d7ba231520331b0954a38f2e518f1

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    EVENT_API_URL: process.env.EVENT_API_URL,
-    USER_API_URL: process.env.USER_API_URL,
-    AUTH_API_URL: process.env.AUTH_API_URL,
+    EVENT_URL: process.env.NEXT_PUBLIC_EVENT_API_URL,
+    USER_URL: process.env.NEXT_PUBLIC_USERS_API_URL,
+    AUTH_URL: process.env.NEXT_PUBLIC_AUTH_API_URL,
   },
 };
 
